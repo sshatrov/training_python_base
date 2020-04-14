2e2a6225f140246ec279596d090aa86a1335bcc1
aeromanager <rigalaiz@gmail.com>
2020-04-14 10:07:34 +0000

    Edit: fixed indent spaces

--- lesson2.py
+++ lesson2.py
@@ -5,8 +5,8 @@ print ("Task 2.1\n")
 m = 5
 i = 0
 while i<m:
-  print (i,0)
-  i+=1
+    print (i,0)
+    i+=1
 
 # 2.2 - Пользователь в цикле вводит 10 цифр. Найти количество введеных пользователем цифр 5.
 print ("\n\nTask 2.2\n")
@@ -14,10 +14,10 @@ i = 0
 ii = 0
 input_str = ''
 while i<10:
-  input_str = input("Enter any number: ")
-  if input_str=="5":
-    ii+=1
-  i+=1
+    input_str = input("Enter any number: ")
+    if input_str=="5":
+      ii+=1
+    i+=1
 print(f"'5' found {ii} times")
 
 #2.3 - Найти сумму ряда чисел от 1 до 100. Полученный результат вывести на экран.
@@ -32,7 +32,7 @@ print(sum)
 print ("\n\nTask 2.4\n")
 sum = 1
 for i in range(2,11):
-  sum*=i
+    sum*=i
 print(sum)
 
 # 2.5 - Вывести цифры числа на каждой строчке.
@@ -50,8 +50,8 @@ print ("\n\nTask 2.6\n")
 integer_number = 2129
 sum = 0
 while integer_number>0:
-  sum+=integer_number%10
-  integer_number = integer_number//10
+    sum+=integer_number%10
+    integer_number = integer_number//10
 print (sum)
 
 # 2.7 - Найти произведение цифр числа.
@@ -60,8 +60,8 @@ integer_number = 2129
 
 sum = 1
 while integer_number>0:
-  sum*=integer_number%10
-  integer_number = integer_number//10
+    sum*=integer_number%10
+    integer_number = integer_number//10
 print (sum)
 
 # 2.8 - Дать ответ на вопрос: есть ли среди цифр числа 5?
@@ -79,9 +79,9 @@ print ("\n\nTask 2.9\n")
 integer_number = 2156
 max_number = 0
 while integer_number>0:
-  if integer_number%10 > max_number:
-    max_number = integer_number%10
-  integer_number = integer_number//10
+    if integer_number%10 > max_number:
+        max_number = integer_number%10
+    integer_number = integer_number//10
 print(max_number)
 
 #2.10 - Найти количество цифр 5 в числе
@@ -89,7 +89,7 @@ print ("\n\nTask 2.10\n")
 integer_number= 51255
 i = 0
 while integer_number>0:
-  if integer_number%10 ==5:
-    i+=1
-  integer_number = integer_number//10
+    if integer_number%10 ==5:
+        i+=1
+    integer_number = integer_number//10
 print(f"'5' found {i} times")
